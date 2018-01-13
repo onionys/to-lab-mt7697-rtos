cfdc175fd28a21523ca521cab6d1f267d4c6b8b7
Tom Lin <tomlin@apk-info.com>
2018-01-13 11:28:28 +0800

    加入第四章節RTOS Thread 設定

--- examples/ch01/HelloWorld-minicom/HelloWorld-minicom.ino
+++ examples/ch01/HelloWorld-minicom/HelloWorld-minicom.ino
@@ -1,15 +1,21 @@
-int loopCount;
+int LED = 7;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  pinMode(LED, OUTPUT);
   
-  loopCount = 0;
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  Serial.println("Hello Welcom to-lab");
-  delay(1000);
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("HIGH");
+  delay(1000);               // wait for a second
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  Serial.println("LOW");
+  delay(1000);               // wait for a second
+  
   
 }
